cb88a723396be3065a34d0ac3e6cbe9455a00b9f
mairandomness <mairakodama@gmail.com>
2018-06-29 18:54:22 -0300

    add README.md

--- rust-lang-book/crackle-pop/src/main.rs
+++ rust-lang-book/crackle-pop/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     for number in 1..101 {
         match number {
-            number if (number % 15 == 0) => println!("CracklePop"),
-            number if (number % 3 == 0) => println!("Crackle"),
-            number if (number % 5 == 0) => println!("Pop"),
+            n if (n % 15 == 0) => println!("CracklePop"),
+            n if (n % 3 == 0) => println!("Crackle"),
+            n if (n % 5 == 0) => println!("Pop"),
             _ => println!("{}", number),
         }
     }
---
+++ space_invader/README.md
@@ -0,0 +1,13 @@
+# Underscore invaders in Rust
+
+This is a small space invaders clone for my application to the Recurse Center.
+
+## How to run
+
+* Install Rust and the [tcod](https://github.com/tomassedovic/tcod-rs#how-to-use-this) library
+* Build and run:
+
+```
+$ cargo build
+$ cargo run
+```
